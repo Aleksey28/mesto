9073193f2f94fa2048b17f09ec088a8c3b84d4df
Aleksey28 <AlekseyPopow1995@yandex.ru>
2020-08-04 17:58:46 +0400

    fix: edit styles

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -11,6 +11,11 @@ const profileProfession = content.querySelector('.profile__profession');
 
 function popupToggle(evt) {
 
+  // Данное уловие срабатывает, если:
+  // - было вызвано событие не из блока попап для открытия блока, на данный момент это относится к кнопке редактирования;
+  // - было вызвано событие из блока на нажатие вне контейнера попап, для закрытия блока;
+  // - было вызвано события из блока на нажатие кнопки закрытия, соответственно для закрытия блока.
+  // Можно подвязать функцию к событиям нажатия только кнопок. Но закрытие попап при нажатии вне контейнера - довольно удобно.
   if (evt.currentTarget !== popup ||
       evt.currentTarget === popup && (evt.currentTarget === evt.target || evt.target === popupBtnClose)) {
     popup.classList.toggle('popup_opend');
@@ -23,11 +28,6 @@ function popupToggle(evt) {
 }
 
 function formSubmitHandler (evt) {
-
-  if (evt.type === 'keypress' && evt.key !== 13) {
-    return;
-  }
-
   evt.preventDefault();
 
   profileName.textContent = popupInputName.value;
@@ -37,5 +37,4 @@ function formSubmitHandler (evt) {
 
 editButton.addEventListener('click', popupToggle);
 popup.addEventListener('click', popupToggle);
-popup.addEventListener('keypress', formSubmitHandler);
 popup.addEventListener('submit', formSubmitHandler);
