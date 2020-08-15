973a186779c583e95734b3a2d4250af339e2776f
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-08-15 20:39:30 +0300

    feat: to show card via script

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
+const initialCards = [
+  {
+      name: 'Хижина чудес',
+      link: './images/card-mystery-shack.jpg',
+      description: 'Хижина в лесу под лучами солнца.'
+  },
+  {
+      name: 'Каньон',
+      link: './images/card-canyon.jpg',
+      description: 'Деревня на дне каньона на закате.'
+  },
+  {
+      name: 'Поместе Нортвест',
+      link: './images/card-northwest-manor.jpg',
+      description: 'Старый и большой особняк под ливнем.'
+  },
+  {
+      name: 'От заката до рассвета',
+      link: './images/card-from-dusk-to-dawn.jpg',
+      description: 'Заброшенный продуктовый магазин.'
+  },
+  {
+      name: 'Остров Задавалы',
+      link: './images/card-island-was-asked.jpg',
+      description: 'Остров посреди озера в тумане.'
+  },
+  {
+      name: 'Город будущего',
+      link: './images/card-future-city.jpg',
+      description: 'Вид на гладиаторскую арену в городе будущего.'
+  }
+];
 const content = document.querySelector('.content');
 const editButton = content.querySelector('.profile__btn_action_edit');
+const cards = content.querySelector('.cards');
+
+
 const popup = document.querySelector('.popup');
 const popupBtnClose = popup.querySelector('.popup__btn_action_close');
-
-const elementCaption = content.querySelector('.element__caption');
-
 const popupInputName = popup.querySelector('.popup__input_type_name');
 const popupInputProfession = popup.querySelector('.popup__input_type_profession');
 const profileName = content.querySelector('.profile__name');
 const profileProfession = content.querySelector('.profile__profession');
+
+
 
 
 function popupToggle(evt) {
@@ -19,7 +53,7 @@ function popupToggle(evt) {
   // - было вызвано события из блока на нажатие кнопки закрытия, соответственно для закрытия блока.
   // Можно подвязать функцию к событиям нажатия только кнопок. Но закрытие попап при нажатии вне контейнера - довольно удобно.
   if (evt.currentTarget !== popup ||
-      evt.currentTarget === popup && (evt.currentTarget === evt.target || evt.target === popupBtnClose)) {
+    evt.currentTarget === popup && (evt.currentTarget === evt.target || evt.target === popupBtnClose)) {
     popup.classList.toggle('popup_opend');
 
     if (popup.classList.contains('popup_opend')) {
@@ -37,6 +71,22 @@ function formSubmitHandler (evt) {
   popup.classList.remove('popup_opend');
 }
 
+const addCards = (cardData) => {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardElementImage = cardElement.querySelector('.card__image');
+  const cardElementCaption = cardElement.querySelector('.card__caption');
+
+  cardElementImage.src = cardData.link;
+  cardElementImage.alt = cardData.description;
+
+  cardElementCaption.textContent = cardData.name;
+
+  cards.append(cardElement);
+}
+
 editButton.addEventListener('click', popupToggle);
 popup.addEventListener('click', popupToggle);
 popup.addEventListener('submit', formSubmitHandler);
+
+initialCards.forEach(addCards);
