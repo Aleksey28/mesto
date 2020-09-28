95973d451d2e54edc56e52ee07014c62ae671f50
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-09-29 00:51:59 +0400

    refactor: removed vendor prefix from css

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,112 @@
-!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t,n){"use strict";n.r(t);n(1);var r=document.querySelector(".content"),o=r.querySelector(".profile__btn_action_edit"),u=r.querySelector(".profile__btn_action_add"),p=Array.from(document.querySelectorAll(".popup")),i={inputSelector:".popup__input",submitButtonSelector:".popup__btn_action_submit",inactiveButtonClass:"popup__btn__disabled",inputErrorClass:"popup__input_type_error",errorClass:"popup__error_visible"},a=document.querySelector(".popup_type_edit"),c=(a.querySelector(".popup__container"),a.querySelector(".popup__input_type_name")),l=a.querySelector(".popup__input_type_profession"),_=new FormValidator(i,a),d=document.querySelector(".popup_type_add"),s=(d.querySelector(".popup__container"),d.querySelector(".popup__input_type_name")),f=d.querySelector(".popup__input_type_link"),y=new FormValidator(i,d),v=r.querySelector(".profile__name"),m=r.querySelector(".profile__profession"),b=r.querySelector(".cards"),S=function(e){e.classList.add("popup_opened"),document.addEventListener("keyup",E)},q=function(e){e.classList.remove("popup_opened"),document.removeEventListener("keyup",E)},E=function(e){if("Escape"===e.key){var t=document.querySelector(".popup_opened");q(t)}},C=function(e){var t=new Card(e,"#card-template",S,".popup_type_show").generateCard();b.prepend(t)};o.addEventListener("click",(function(){S(a),c.value=v.textContent,l.value=m.textContent,_.resetValidationForForm()})),u.addEventListener("click",(function(){S(d),s.value="",f.value="",y.resetValidationForForm()})),_.enableValidation(),y.enableValidation(),a.addEventListener("submit",(function(e){e.preventDefault(),v.textContent=c.value,m.textContent=l.value,q(a)})),d.addEventListener("submit",(function(e){e.preventDefault(),C({name:s.value,link:f.value,description:s.value}),q(d)})),p.forEach((function(e){var t=e.querySelector(".popup__btn_action_close");e.addEventListener("click",(function(t){t.target===t.currentTarget&&q(e)})),t.addEventListener("click",(function(){return q(e)}))})),initialCards.forEach(C)},function(e,t,n){}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/scripts/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/pages/index.css":
+/*!*****************************!*\
+  !*** ./src/pages/index.css ***!
+  \*****************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./src/pages/index.css?");
+
+/***/ }),
+
+/***/ "./src/scripts/index.js":
+/*!******************************!*\
+  !*** ./src/scripts/index.js ***!
+  \******************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _pages_index_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../pages/index.css */ \"./src/pages/index.css\");\n/* harmony import */ var _pages_index_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_pages_index_css__WEBPACK_IMPORTED_MODULE_0__);\n\nvar content = document.querySelector(\".content\");\nvar editButton = content.querySelector(\".profile__btn_action_edit\");\nvar addButton = content.querySelector(\".profile__btn_action_add\");\nvar popupArr = Array.from(document.querySelectorAll(\".popup\"));\nvar validationSettings = {\n  inputSelector: \".popup__input\",\n  submitButtonSelector: \".popup__btn_action_submit\",\n  inactiveButtonClass: \"popup__btn__disabled\",\n  inputErrorClass: \"popup__input_type_error\",\n  errorClass: \"popup__error_visible\"\n};\nvar popupEdit = document.querySelector(\".popup_type_edit\");\nvar popupEditContainer = popupEdit.querySelector(\".popup__container\");\nvar popupEditInputName = popupEdit.querySelector(\".popup__input_type_name\");\nvar popupEditInputProfession = popupEdit.querySelector(\".popup__input_type_profession\");\nvar popupEditValidator = new FormValidator(validationSettings, popupEdit);\nvar popupAdd = document.querySelector(\".popup_type_add\");\nvar popupAddContainer = popupAdd.querySelector(\".popup__container\");\nvar popupAddInputName = popupAdd.querySelector(\".popup__input_type_name\");\nvar popupAddInputlink = popupAdd.querySelector(\".popup__input_type_link\");\nvar popupAddValidator = new FormValidator(validationSettings, popupAdd);\nvar profileName = content.querySelector(\".profile__name\");\nvar profileProfession = content.querySelector(\".profile__profession\");\nvar cards = content.querySelector(\".cards\");\n\nvar openPopup = function openPopup(popupToOpen) {\n  popupToOpen.classList.add(\"popup_opened\");\n  document.addEventListener(\"keyup\", closeOpenedPopup);\n};\n\nvar closePopup = function closePopup(popupToClose) {\n  popupToClose.classList.remove(\"popup_opened\");\n  document.removeEventListener(\"keyup\", closeOpenedPopup);\n};\n\nvar closeOpenedPopup = function closeOpenedPopup(evt) {\n  if (evt.key === \"Escape\") {\n    var openedPopup = document.querySelector(\".popup_opened\");\n    closePopup(openedPopup);\n  }\n};\n\nvar addCards = function addCards(cardData) {\n  var cardElement = new Card(cardData, \"#card-template\", openPopup, \".popup_type_show\").generateCard();\n  cards.prepend(cardElement);\n};\n\nvar submitPopupEdit = function submitPopupEdit(evt) {\n  evt.preventDefault();\n  profileName.textContent = popupEditInputName.value;\n  profileProfession.textContent = popupEditInputProfession.value;\n  closePopup(popupEdit);\n};\n\nvar submitPopupAdd = function submitPopupAdd(evt) {\n  evt.preventDefault();\n  addCards({\n    name: popupAddInputName.value,\n    link: popupAddInputlink.value,\n    description: popupAddInputName.value\n  });\n  closePopup(popupAdd);\n};\n\neditButton.addEventListener(\"click\", function () {\n  openPopup(popupEdit);\n  popupEditInputName.value = profileName.textContent;\n  popupEditInputProfession.value = profileProfession.textContent; //Если не сделать reset то:\n  //1. кнопка отправки будет заблокирована в любом случае, так как на момент инициализации валидации поля попап не заполнены.\n  //2. В случае если оставить форму с ошибками и закрыть, то при открытии ошибки останутся, а данные будут перезаполнены.\n\n  popupEditValidator.resetValidationForForm();\n});\naddButton.addEventListener(\"click\", function () {\n  openPopup(popupAdd);\n  popupAddInputName.value = \"\";\n  popupAddInputlink.value = \"\";\n  popupAddValidator.resetValidationForForm();\n});\npopupEditValidator.enableValidation();\npopupAddValidator.enableValidation();\npopupEdit.addEventListener(\"submit\", submitPopupEdit);\npopupAdd.addEventListener(\"submit\", submitPopupAdd);\npopupArr.forEach(function (popupElement) {\n  var popupBtnClose = popupElement.querySelector(\".popup__btn_action_close\");\n  popupElement.addEventListener(\"click\", function (evt) {\n    if (evt.target === evt.currentTarget) {\n      closePopup(popupElement);\n    }\n  });\n  popupBtnClose.addEventListener(\"click\", function () {\n    return closePopup(popupElement);\n  });\n});\ninitialCards.forEach(addCards);\n\n//# sourceURL=webpack:///./src/scripts/index.js?");
+
+/***/ })
+
+/******/ });
