ae52437cd6ae6efe936bafdd215140a988f1bde2
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-09-29 21:38:59 +0400

    refactor: added metod _getInputValues

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -4,5 +4,14 @@ export default class PopupWithForm extends Popup {
   constructor(selector, handlerSubmit) {
     super(selector);
     this._handlerSubmit = handlerSubmit;
+    this._popupInputName = this._popupElement.querySelector(".popup__input_type_name");
+    this._popupInputlink = this._popupElement.querySelector(".popup__input_type_link");
+    this._popupInputProfession = this._popupElement.querySelector(
+      ".popup__input_type_profession"
+    );
+  }
+
+  _getInputValues() {
+    return;
   }
 }
