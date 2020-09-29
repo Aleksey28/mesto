9add86df3bbae68b11f751822cd7a85386352c8f
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-09-29 21:30:43 +0400

    refactor: PopupWithForm class constuctor was added

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,5 +1,8 @@
 import Popup from "./Popup.js";
 
 export default class PopupWithForm extends Popup {
-
+  constructor(selector, handlerSubmit) {
+    super(selector);
+    this._handlerSubmit = handlerSubmit;
+  }
 }
