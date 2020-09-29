20236d189925ae31efb1a24765ff8f1f859d1f24
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-09-29 21:24:08 +0400

    refactor: added metod for class PopupWithImage

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,7 +1,7 @@
 import Popup from "./Popup.js";
 
 export default class PopupWithImage extends Popup {
-  open(link, description, name) {
+  open({link, name, description}) {
     const showPopupImage = this._popupElement.querySelector(".popup__image");
     const showPopupCaption = this._popupElement.querySelector(".popup__caption");
 
