53a4656bfcb7a0b7d56b78d2441e87a84781bddb
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-09-29 20:51:06 +0400

    refactor: added metod setEventListeners

--- src/scripts/components/Popup.js
+++ src/scripts/components/Popup.js
@@ -1,6 +1,8 @@
 export default class Popup {
   constructor(selector) {
     this._popupElement = document.querySelector(selector);
+    this.close = this.close.bind(this);
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {
@@ -11,9 +13,25 @@ export default class Popup {
     this._popupElement.classList.remove("popup_opened");
   }
 
-  _handleEscClose() {
+  _handleEscClose(evt) {
     if (evt.key === "Escape") {
       this.close();
     }
+  }
+
+  setEventListeners() {
+    this._popupBtnClose = this._popupElement.querySelector(
+      ".popup__btn_action_close"
+    );
+
+    this._popupElement.addEventListener("click", (evt) => {
+      if (evt.target === evt.currentTarget) {
+        this.close();
+      }
+    });
+
+    this._popupBtnClose.addEventListener("click", () => this.close());
+
+    document.addEventListener("keyup", _handleEscClose);
   }
 }
