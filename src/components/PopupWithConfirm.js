074e85401b10b73a6d7a1e9db6685b515060f2ba
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-10-08 22:36:29 +0400

    feat: added class PopupWithConfirm in js

---
+++ src/components/PopupWithConfirm.js
@@ -0,0 +1,31 @@
+import Popup from "./Popup.js";
+
+export default class PopupWithForm extends Popup {
+  constructor(selector, { handlerSubmit, handlerOpen }) {
+    super(selector);
+    this._handlerSubmit = handlerSubmit;
+    this._handlerOpen = handlerOpen;
+  }
+
+  _handleSubmitPopup(evt) {
+    evt.preventDefault();
+    this.close();
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._popupElement.addEventListener(
+      "submit",
+      this._handleSubmitPopup.bind(this)
+    );
+  }
+
+  open() {
+    super.open();
+    this._handlerOpen();
+  }
+
+  close() {
+    super.close();
+  }
+}
