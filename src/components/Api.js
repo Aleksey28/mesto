503f994cd9f7094e38aba5c9413027b639ba6ff8
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-10-13 13:54:14 +0400

    refactor: refactor class api

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,81 +1,72 @@
 import { data } from "autoprefixer";
 
 export default class Api {
-  constructor({ url, idGroup, token }) {
-    this._url = url;
-    this._idGroup = idGroup;
-    this._token = token;
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
   }
 
-  _getProxy(relativePath, method, headers = {}, body = "") {
+  _getProxy(relativePath, method, body = "") {
     const options = {
       method,
-      headers,
+      headers: this._headers,
     };
-
-    options.headers.authorization = this._token;
 
     if (!!body) {
       options.body = body;
     }
 
-    return fetch(`${this._url}/v1/${this._idGroup}${relativePath}`, options);
+    return fetch(`${this._baseUrl}${relativePath}`, options);
+  }
+
+  async _handleResponse(response) {
+    if (response.ok) {
+      return await response.json();
+    } else {
+      return Promise.reject(`Ошибка: ${response.status}`);
+    }
   }
 
   async getUserData() {
     const response = await this._getProxy("/users/me", "GET");
-    const data = await response.json();
-
-    console.log(data);
+    return await this._handleResponse(response);
   }
 
   async setUserData({ name, about }) {
     const response = await this._getProxy(
       "/users/me",
       "PATCH",
-      { "Content-Type": "application/json" },
       JSON.stringify({ name, about })
     );
-    return await response.json();
+    return await this._handleResponse(response);
   }
 
   async setAvatar({ link }) {
     const response = await this._getProxy(
       "/users/me/avatar",
       "PATCH",
-      { "Content-Type": "application/json" },
       JSON.stringify({ avatar: link })
     );
-
-    return await response.json();
+    return await this._handleResponse(response);
   }
 
   async getCardList() {
     const response = await this._getProxy("/cards", "GET");
-    const data = await response.json();
-
-    console.log(data);
+    return await this._handleResponse(response);
   }
 
   async addCard({ name, link }) {
     const response = await this._getProxy(
       "/cards",
       "POST",
-      { "Content-Type": "application/json" },
       JSON.stringify({ name, link })
     );
-
-    const data = await response.json();
-
-    console.log(data);
+    return await this._handleResponse(response);
   }
 
   async deleteCard(id) {
     const response = await this._getProxy(`/cards/${id}`, "DELETE");
-
-    const data = await response.json();
-
-    console.log(data);
+    return await this._handleResponse(response);
   }
 
   addCardLike(id) {
@@ -88,9 +79,6 @@ export default class Api {
 
   async _editCardLike(id, action) {
     const response = await this._getProxy(`/cards/likes/${id}`, action);
-
-    const data = await response.json();
-
-    console.log(data);
+    return await this._handleResponse(response);
   }
 }
